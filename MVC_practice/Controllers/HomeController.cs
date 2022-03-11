33bf689ca59fbccd12290a5f976708bf81b4ee67
Jerry-Chang975 <jerry975607@gmail.com>
2022-03-11 14:04:28 +0800

    練習從前端取得資料

--- MVC_practice/Controllers/HomeController.cs
+++ MVC_practice/Controllers/HomeController.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_practice.Models;
 
 namespace MVC_practice.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
-            return View();
-        }
+            //DateTime dt = DateTime.Now;
+            //Student data = new Student();
+            //List<Student> st_list = new List<Student>();
+            //st_list.Add(new Student("1", "jerry", 90));
+            //st_list.Add(new Student("2", "Miigun", 80));
+            //st_list.Add(new Student("3", "Miibo", 99));
 
-        public ActionResult About()
+            //ViewBag.Date = dt;
+            //ViewBag.Student = data;
+            //ViewBag.List = st_list;
+
+            //return View();
+
+            //--------------------------
+            DateTime date = DateTime.Now;
+            ViewBag.Date = date;
+
+            Student data = new Student("1", "jerry", 90);
+            return View(data);
+        }
+        public ActionResult Transcripts(string id, string name, int score)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Student data = new Student(id, name, score);
+            return View(data);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+    }   
 }
